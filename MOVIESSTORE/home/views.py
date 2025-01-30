789eff1d7dc7b80a4ebcae596d3453d8d5d66756
stevennhaa <ha.steven.pa@gmail.com>
2025-01-30 15:47:21 -0500

    initial commit

--- MOVIESSTORE/home/views.py
+++ MOVIESSTORE/home/views.py
@@ -8,4 +8,4 @@ def about(request):
     template_data['title'] = 'About'
     return render(request, 'home/about.html',
                 {'template_data': template_data})
-#initial commit steven ha
+#initial commit hi michael
