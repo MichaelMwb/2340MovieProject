3f2e551650e18acd3e807a76c8caa6557c604bfc
Michael <Michaelwbell05@hotmail.com>
2025-01-31 13:29:37 -0500

    Merge branch 'main' into michael

--- MOVIESSTORE/movies/urls.py
+++ MOVIESSTORE/movies/urls.py
@@ -1,17 +1,5 @@
 from django.urls import path
 from . import views
-<<<<<<< HEAD
-urlpatterns = [
-    path('', views.index, name='movies.index'),
-    path('<int:id>/', views.show, name='movies.show'),
-    path('<int:id>/review/create/', views.create_review,
-        name='movies.create_review'),
-    path('<int:id>/review/<int:review_id>/edit/',
-        views.edit_review, name='movies.edit_review'),
-    path('<int:id>/review/<int:review_id>/delete/',
-        views.delete_review, name='movies.delete_review'),
-]
-=======
 
 urlpatterns = [
     path('', views.index, name='movies.index'),
@@ -20,4 +8,3 @@ urlpatterns = [
     path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
     path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='movies.delete_review'),
 ]
->>>>>>> 34cdd02e7b63105c3685ae56cab15bcfcdf10102
