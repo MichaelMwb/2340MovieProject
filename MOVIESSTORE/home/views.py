1691c138354522469d73089f3925b9ad4d85d3e6
stevennhaa <ha.steven.pa@gmail.com>
2025-02-01 11:39:57 -0500

    Chapter 12
    commit

--- MOVIESSTORE/home/views.py
+++ MOVIESSTORE/home/views.py
@@ -8,4 +8,3 @@ def about(request):
     template_data['title'] = 'About'
     return render(request, 'home/about.html',
                 {'template_data': template_data})
-#initial commit hi michael
