3e70f2d2c8a187f480c658bdbc12087dbcd02c7f
Melanie Chen <ychen.melanie@gmail.com>
2025-02-12 06:18:15 -0500

    Def points to sign up + route between

--- MOVIESSTORE/moviesstore/urls.py
+++ MOVIESSTORE/moviesstore/urls.py
@@ -18,14 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from accounts import views as accounts_views  # Import the signup view directly
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', accounts_views.signup, name='default_signup'),  # Default path is now signup
+    path('home/', include('home.urls')),  # Move home under '/home/'
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
