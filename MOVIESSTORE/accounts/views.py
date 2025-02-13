9dbf872aa2d9831b319598a7db852ea35c1717f2
Melanie Chen <140217606+ChenMel27@users.noreply.github.com>
2025-02-13 15:33:16 -0500

    Reverting

--- MOVIESSTORE/accounts/views.py
+++ MOVIESSTORE/accounts/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 @login_required
 def logout(request):
     auth_logout(request)
-    return redirect('home.index')
+    return redirect('movies.index')
 
 def login(request):
     template_data = {}
@@ -23,7 +23,7 @@ def login(request):
             return render(request, 'accounts/login.html', {'template_data': template_data})
         else:
             auth_login(request, user)
-            return redirect('home.index')
+            return redirect('movies.index')
 
 def signup(request):
     template_data = {}
