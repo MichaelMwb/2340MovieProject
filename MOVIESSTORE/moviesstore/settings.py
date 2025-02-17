6b0a32e035bc1a7027d8a4e2dda748aad3e41aab
Michael <Michaelwbell05@hotmail.com>
2025-02-17 12:41:10 -0500

    Settings

--- MOVIESSTORE/moviesstore/settings.py
+++ MOVIESSTORE/moviesstore/settings.py
@@ -129,16 +129,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Add this line to set the STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optionally, you can also set STATICFILES_DIRS if you have additional static files directories
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'moviesstore/static/',
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
